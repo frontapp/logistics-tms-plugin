fc5a8667f4a6e0aec55f52db8ec2e8178a20b7cc
jrobalino <javier.robalino@gmail.com>
2023-08-29 14:10:10 -0500

    Remove unnecessary config and add in CSS that was deleted.

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    loader: "tsx",
-    include: [
-      "src/**/*.jsx",
-      "src/**/*.tsx",
-      "node_modules/**/*.jsx",
-      "node_modules/**/*.tsx",
-    ]
-  },
   plugins: [react()],
 })
